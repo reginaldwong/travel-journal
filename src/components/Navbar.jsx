a55a0b76d9793a85ae83594f145030cb61f10054
Reginald Wong <reginald_wong@outlook.com>
2023-02-12 23:45:47 -0500

    style navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const Navbar = () => {
     return (
-        <h1>Navbar goes here</h1>
+        <nav className="navbar--nav"><img src="/public/assets/globe.png" alt="globe icon" className="navbar--icon" />journeys abroad</nav>
     )
 }
 
