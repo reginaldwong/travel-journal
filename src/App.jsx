848928442d33ca3880baafd1742cc40f183aeea2
Reginald Wong <reginald_wong@outlook.com>
2023-02-12 23:25:34 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Navbar from './components/Navbar'
+import Card from './components/Card'
+import Footer from './components/Footer'
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Card />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Card = () => {
+    return (
+        <h1>Card goes here</h1>
+    )
+}
+
+export default Card;
