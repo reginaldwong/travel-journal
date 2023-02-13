6abe0d25a072c89fe5f5b5cbb91daea7d5a52c68
Reginald Wong <reginald_wong@outlook.com>
2023-02-13 09:31:23 -0500

    edit Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,12 @@ import React from "react"
 
 const Card = (props) => {
     return (
-        <div>
-            <img src={props.imageUrl} alt="" />
-            <h1>{props.title}</h1>
+        <div className="card">
+            <img src={props.item.imageUrl} alt="" />
+            <h3>{props.item.location}</h3> <a href={props.item.googleMapsUrl}>View on Google Maps</a>
+            <h2>{props.item.title}</h2>
+            <p>{props.item.startDate} - {props.item.endDate}</p>
+            <p>{props.item.description}</p>
         </div>
     )
 }
