9dd77d55a36780ae1a822f744a45115914f51f7a
Reginald Wong <reginald_wong@outlook.com>
2023-02-14 00:26:17 -0500

    add data and style card component

--- src/data.js
+++ src/data.js
@@ -1,5 +1,28 @@
 export default [
   {
+    id: 1,
+    title: "Phang Nga Bay",
+    location: "Phuket",
+    googleMapsUrl: "https://goo.gl/maps/wNfHQSJwQCHZVh1s5",
+    startDate: "08 Feb, 2018",
+    endDate: "10 Feb, 2018",
+    description:
+      "Waterway for kayak tours & boat excursions, plus views of nearby cliffs, caves & rock formations.",
+    imageUrl: "/public/assets/phuket.jpg",
+  },
+  {
+    id: 2,
+    title: "Cayo Coco",
+    location: "Cuba",
+    googleMapsUrl: "https://goo.gl/maps/UvVvWFm3rykD2oav9",
+    startDate: "09 Nov, 2018",
+    endDate: "15 Nov, 2018",
+    description:
+      "Tropical island dotted with all-inclusive resorts, white-sand beaches, coral reefs and lagoons.",
+    imageUrl: "/public/assets/cuba.jpg",
+  },
+  {
+    id: 3,
     title: "Mount Royal",
     location: "Montreal",
     googleMapsUrl: "https://goo.gl/maps/QwaiGW3NgPJLwFfM8",
@@ -10,6 +33,7 @@ export default [
     imageUrl: "/public/assets/montreal.jpg",
   },
   {
+    id: 4,
     title: "Victoria Peak",
     location: "Hong Kong",
     googleMapsUrl: "https://goo.gl/maps/KZHLHy5F2xGxgcMw7",
@@ -18,5 +42,38 @@ export default [
     description:
       "Mountain on Hong Kong island (elevation 1,181 feet) known for its skyline & waterfront views.",
     imageUrl: "/public/assets/hongkong.jpg",
+  },
+  {
+    id: 5,
+    title: "Ruins of St. Paul's",
+    location: "Macau",
+    googleMapsUrl: "https://goo.gl/maps/TeWVpkGWXHpp8cxJ6",
+    startDate: "20 Mar, 2019",
+    endDate: "21 Mar, 2019",
+    description:
+      "Well-known facade of a 17th-century church destroyed by fire, with an on-site religious museum.",
+    imageUrl: "/public/assets/macau.jpg",
+  },
+  {
+    id: 6,
+    title: "DisneyWorld",
+    location: "Orlando",
+    googleMapsUrl: "https://goo.gl/maps/c4G53tF7XC6J2ygz8",
+    startDate: "04 Sept, 2022",
+    endDate: "11 Sept, 2022",
+    description:
+      "Well-known attraction comprising of large resorts, dining, shops & 4 theme parks with rides & shows.",
+    imageUrl: "/public/assets/disneyworld.jpg",
+  },
+  {
+    id: 7,
+    title: "The Round Tower",
+    location: "Copenhagen",
+    googleMapsUrl: "https://goo.gl/maps/3hZ9gp5CVGgpV7Ag7",
+    startDate: "05 Dec, 2022",
+    endDate: "06 Dec, 2022",
+    description:
+      "17th-century tower with an observatory, planetarium, event hall & spiral ramp instead of stairs.",
+    imageUrl: "/public/assets/copenhagen.jpg",
   },
 ];
