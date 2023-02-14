1fa181f1875ff704df348eee6cb192b594c42d93
Reginald Wong <reginald_wong@outlook.com>
2023-02-13 19:25:39 -0500

    edit card styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,11 +3,15 @@ import React from "react"
 const Card = (props) => {
     return (
         <div className="card">
-            <img src={props.item.imageUrl} alt="" />
-            <h3>{props.item.location}</h3> <a href={props.item.googleMapsUrl}>View on Google Maps</a>
-            <h2>{props.item.title}</h2>
-            <p>{props.item.startDate} - {props.item.endDate}</p>
-            <p>{props.item.description}</p>
+            <img src={props.item.imageUrl} alt="" className="card--image" />
+            <div className="card--stats">
+            <div className="card--location"> <img src="/public/assets/pin.png" alt="pin" /> <h2>{props.item.location} </h2>
+                <a href={props.item.googleMapsUrl} className="card--link" target="_blank">View on Google Maps</a>
+            </div> 
+            <h2 className="card--title">{props.item.title}</h2>
+            <p className="card--dates">{props.item.startDate} - {props.item.endDate}</p>
+            <p className="card--description">{props.item.description}</p>
+            </div>
         </div>
     )
 }
