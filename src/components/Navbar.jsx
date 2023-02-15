f39c14fc16e22982e0e969e61cfa0de8eb97d06a
Reginald Wong <reginald_wong@outlook.com>
2023-02-14 20:59:22 -0500

    edit navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,10 @@ import React from "react"
 
 const Navbar = () => {
     return (
-        <nav className="navbar--nav"><img src="/public/assets/globe.png" alt="globe icon" className="navbar--icon" />journeys abroad</nav>
+        <nav className="navbar--nav">
+        <img src="/public/assets/globe.png" alt="globe icon" className="navbar--icon" />
+        <p>journeys abroad | <span className="navbar--subtitle">a travel journal by reginald wong</span> </p>   
+        </nav>
     )
 }
 
