6cd31e5588b8458ae3ae2e41f9e8b6fffec454d0
Reginald Wong <reginald_wong@outlook.com>
2023-02-16 08:51:59 -0500

    add media query for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <nav className="navbar--nav">
         <img src="/public/assets/globe.png" alt="globe icon" className="navbar--icon" />
-        <p>journeys abroad | <span className="navbar--subtitle">a travel journal by reginald wong</span> </p>   
+        <p className="navbar--title">journeys abroad | <span className="navbar--subtitle">a travel journal by reginald wong</span> </p>   
         </nav>
     )
 }
